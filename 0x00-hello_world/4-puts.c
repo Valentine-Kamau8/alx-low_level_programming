0e273fb57b1ee7258e789ce89c93ca41392f4e8f
valentinekamau <kamauvalentine8@gmail.com>
2022-09-10 15:47:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+
+   * main - Begins here
+
+    *
+
+     * Return: 0
+
+      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
